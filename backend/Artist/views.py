b2178e278df0e5f2a188012ce4264d0a292c81de
Mach1r0 <segundacontafd@gmail.com>
2024-09-07 11:42:09 -0300

    feat: making the Login page and artist page

--- backend/Artist/views.py
+++ backend/Artist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Artist
 from .serializer import ArtistSerializer
 
---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Style from '../style/login.module.css'
+
+export default function page() {
+  return (
+    <div className={Style['container-all']}>
+        <div className={Style['container-form']}>
+            <div className={Style['container-form-header']}>
+                <h1>Login</h1>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
