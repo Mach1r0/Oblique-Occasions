dab06989b893dbe695a5d5e96663bd8f74de836c
mach1r0 <segundacontafd@gmail.com>
2024-09-18 10:54:24 -0300

    fix: :bug: Fixing the install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ docker-compose build
 docker-compose up -d
 
 echo "Aguardando o banco de dados iniciar..."
-sleep 10
+sleep 20
+
+until docker-compose exec postgres pg_isready -U myuser -d mydb -h postgres; do
+  echo "Esperando o banco de dados ficar pronto..."
+  sleep 5
+done
 
 echo "Executando migrações do Django..."
 docker-compose exec backend python manage.py migrate
@@ -19,8 +24,11 @@ docker-compose exec backend sh -c "mkdir -p /app/media"
 echo "Extraindo arquivos de mídia..."
 docker-compose exec backend sh -c "tar -xzvf /app/media_backup.tar.gz -C /app/media --strip-components=1"
 
+# Remover diretórios desnecessários (se necessário)
 docker-compose exec backend sh -c "rm -rf /app/media/media"
 
+# Reiniciar os contêineres para garantir que todas as configurações sejam aplicadas
 docker-compose restart
 
+# Mensagem de conclusão
 echo "Instalação concluída com sucesso!"
